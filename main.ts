04b785df0d3595e23543a39f51b58369fa702ab4
hokarimasahiro <42521359+hokarimasahiro@users.noreply.github.com>
2021-06-28 09:47:05 +0900

    更新 pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -16,21 +16,25 @@ function 着信表示 (時間: number, 電話番号: number) {
     basic.pause(200)
     neopixel.showColor(neopixel.colors(neopixel.Colors.Black))
 }
+bluetooth.onUartDataReceived(serial.delimiters(Delimiters.NewLine), function () {
+    QUEUE.push(bluetooth.uartReadUntil(serial.delimiters(Delimiters.NewLine)))
+})
 function コマンド処理 () {
-    if (コマンド == "s") {
-        rtc.setClockData(clockData.year, parseInt(パラメータ[0]))
-        rtc.setClockData(clockData.month, parseInt(パラメータ[1]))
-        rtc.setClockData(clockData.day, parseInt(パラメータ[2]))
-        rtc.setClockData(clockData.weekday, parseInt(パラメータ[3]))
-        rtc.setClockData(clockData.hour, parseInt(パラメータ[4]))
-        rtc.setClockData(clockData.minute, parseInt(パラメータ[5]))
-        rtc.setClockData(clockData.second, parseInt(パラメータ[6]))
+    コマンド = 受信文字.split(",")
+    if (コマンド[0] == "s") {
+        rtc.setClockData(clockData.year, parseInt(コマンド[1]))
+        rtc.setClockData(clockData.month, parseInt(コマンド[2]))
+        rtc.setClockData(clockData.day, parseInt(コマンド[3]))
+        rtc.setClockData(clockData.weekday, parseInt(コマンド[4]))
+        rtc.setClockData(clockData.hour, parseInt(コマンド[5]))
+        rtc.setClockData(clockData.minute, parseInt(コマンド[6]))
+        rtc.setClockData(clockData.second, parseInt(コマンド[7]))
         rtc.setClock()
         時刻表示(0)
-    } else if (コマンド == "a") {
-        pins.analogPitch(parseInt(パラメータ[0]), parseInt(パラメータ[1]))
-    } else if (コマンド == "v") {
-        着信表示(parseInt(パラメータ[0]), parseInt(パラメータ[1]))
+    } else if (コマンド[0] == "a") {
+        pins.analogPitch(parseInt(コマンド[1]), parseInt(コマンド[2]))
+    } else if (コマンド[0] == "v") {
+        着信表示(parseInt(コマンド[1]), parseInt(コマンド[2]))
     }
 }
 function 表示方向 () {
@@ -61,28 +65,26 @@ function 時刻表示 (タイプ: number) {
         basic.clearScreen()
         basic.pause(500)
     } else if (タイプ == 1) {
-        atp3012.write("tada'ima <NUMK VAL=" + rtc.getClockData(clockData.hour) + " COUNTER=ji>" + " <NUMK VAL=" + rtc.getClockData(clockData.minute) + " COUNTER=funn>desu.")
         basic.showString("" + rtc.getClockData(clockData.hour) + ":" + rtc.getClockData(clockData.minute))
     }
 }
-let パラメータ: string[] = []
-let コマンド = ""
+let 受信文字 = ""
+let コマンド: string[] = []
+let QUEUE: string[] = []
 neopixel.initNeopixel(DigitalPin.P1, 4)
 neopixel.showColor(neopixel.colors(neopixel.Colors.Black))
 pins.digitalWritePin(DigitalPin.P2, 0)
 pins.setPull(DigitalPin.P8, PinPullMode.PullUp)
 pins.setPull(DigitalPin.P12, PinPullMode.PullUp)
 pins.setPull(DigitalPin.P13, PinPullMode.PullUp)
-let 消灯時間 = 600
-let QUEUE: string[] = []
+QUEUE = []
 bluetooth.startUartService()
 rtc.getClock()
 時刻表示(0)
 basic.forever(function () {
     basic.pause(100)
     if (QUEUE.length > 0) {
-        コマンド = QUEUE[0].split(",")[0]
-        パラメータ = QUEUE[0].substr(2, 100).split(",")
+        受信文字 = QUEUE.removeAt(0)
         コマンド処理()
         QUEUE.shift()
     }
@@ -93,18 +95,13 @@ basic.forever(function () {
         pins.digitalWritePin(DigitalPin.P2, 0)
         basic.pause(800)
     }
-    if (input.buttonIsPressed(Button.A) && !(input.buttonIsPressed(Button.B))) {
+    if (input.buttonIsPressed(Button.A)) {
         時刻表示(1)
-    } else if (input.buttonIsPressed(Button.B) && !(input.buttonIsPressed(Button.A))) {
+    } else if (input.buttonIsPressed(Button.B)) {
         秒表示()
     } else if (input.isGesture(Gesture.Shake)) {
-        時刻表示(0)
-    } else if (input.logoIsPressed()) {
         時刻表示(0)
     } else {
         basic.clearScreen()
     }
-})
-basic.forever(function () {
-    QUEUE.push(bluetooth.uartReadUntil(serial.delimiters(Delimiters.NewLine)))
 })
